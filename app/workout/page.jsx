596abc2532a0d8e1f81afb4af1bfdfd1b6a15f0b
Brian Wyatt <brianlwyatt@gmail.com>
2023-04-16 15:49:42 -0300

    functioning 3rd party API search

--- app/workout/page.jsx
+++ app/workout/page.jsx
@@ -1,37 +1,57 @@
-import ExerciseCard from '../components/ExerciseCard'
+'use client';
 
-const fetchRepo = async () => {
-  const userInput = 'biceps'
+import { useState } from 'react';
+import ExerciseCard from '../components/ExerciseCard';
+import Hero from '../assets/barbell.jpg';
+import './WorkoutStyles.css';
+import Image from 'next/image';
 
-  const data = await fetch (`https://api.api-ninjas.com/v1/exercises?muscle=${userInput}`, 
-    {
-      headers: {'X-Api-Key': 'G2eNCdx3VuH8TLfQGT8EpQ==KhaMg8wKYcZYoA40'}
-    })
-  const repo = await data.json()
-  return repo;
-}
+const WorkoutPage = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [repo, setRepo] = useState([]);
 
-const WorkoutPage = async () => {
-  const repo = await fetchRepo();
-  console.log(repo);
-  
+  const handleSearch = async () => {
+    if (!searchInput) return;
+    const userInput = searchInput.trim();
+    const data = await fetch(
+      `https://api.api-ninjas.com/v1/exercises?muscle=${userInput}`,
+      {
+        headers: { 'X-Api-Key': 'G2eNCdx3VuH8TLfQGT8EpQ==KhaMg8wKYcZYoA40' },
+      }
+    );
+    const repoData = await data.json();
+    setRepo(repoData);
+  };
+
   return (
-    <div className="workout-container">
-      <div className="exercises">
-        {repo.map((val, index) => {
-          return(
-            <ExerciseCard
-              key={index}
-              name={val.name}
-              type={val.type}
-              muscle={val.muscle}
-            />
-          )
-        })}
+    <div className="workout-page">
+      <div>
+        <Image className="planner-hero" src={Hero} alt="Loading a heavy barbell." />
       </div>
-    </div>  
-  )
-}
+      <div className="workout-container">
+        <div className="col-1">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search exercises"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="exercises">
+            {repo && repo.map((val, index) => {
+              return (
+                <ExerciseCard key={index} name={val.name} type={val.type} muscle={val.muscle} />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WorkoutPage
-export { fetchRepo };
+export default WorkoutPage;
