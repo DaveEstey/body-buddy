7e0e6a35db3c411096b7cf7ad17d0036c8c697a3
Max <max.belenchuk@gmail.com>
2023-04-17 20:41:18 -0300

    Attempting call backs

--- pages/api/restricted.js
+++ pages/api/restricted.js
@@ -15,3 +15,21 @@ export default async (req, res) => {
     })
   }
 }
+
+// callbacks: {
+//     async jwt({ token, account }) {
+//       // Persist the OAuth access_token to the token right after signin
+//       if (account) {
+//         token.accessToken = account.access_token
+//       }
+//       return token
+//     },
+//     async session({ session, token, user }) {
+//       // Send properties to the client, like an access_token from a provider.
+//       session.accessToken = token.accessToken
+//       return session
+//     }
+//   }
+
+
+// Come back to this later
