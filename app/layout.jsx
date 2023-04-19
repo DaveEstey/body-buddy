dafd61f37c7b6d0657bfa5115b60e6369a93e79b
Max <max.belenchuk@gmail.com>
2023-04-19 18:21:44 -0300

    Made changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,14 @@
+
 import './globals.css';
 import NavBar from './components/NavBar';
+
+
+
+
+
+ 
+
+
 
 export const metadata = {
   title: 'Body Buddy - Your Digital Workout Planner',
@@ -7,15 +16,26 @@ export const metadata = {
   keywords: "body, workout, gym, exercise, fitness, planner"
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({children}) {
+  
   return (
+    
     <html lang="en">
       <body>
+    
         <NavBar />
+      
         <main suppressHydrationWarning={true}>
-          {children}
+       {children}
+       
         </main>
       </body>
     </html>
+    
   )
 }
+
+
+
+
+
