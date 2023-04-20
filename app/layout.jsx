99bba24699cd53349520292bf73e614bb1e0f9f7
Max <max.belenchuk@gmail.com>
2023-04-20 15:22:00 -0300

    layout.jsx wrapped with session provider to match nextauth

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,13 +2,7 @@
 import './globals.css';
 import NavBar from './components/NavBar';
 
-
-
-
-
- 
-
-
+import EmailProvider from 'next-auth/providers/email'
 
 export const metadata = {
   title: 'Body Buddy - Your Digital Workout Planner',
@@ -16,24 +10,42 @@ export const metadata = {
   keywords: "body, workout, gym, exercise, fitness, planner"
 }
 
+export const authOptions = {
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    // Add more providers here as needed
+  ],
+}
+
 export default function RootLayout ({children}) {
   
   return (
-    
     <html lang="en">
       <body>
-    
         <NavBar />
-      
-        <main suppressHydrationWarning={true}>
-       {children}
-       
-        </main>
+        {typeof window !== 'undefined' && (
+          <SessionProvider session={metadata}>
+            <main suppressHydrationWarning={true}>
+              {children}
+            </main>
+          </SessionProvider>
+        )}
       </body>
     </html>
-    
   )
 }
+
+
 
 
 
