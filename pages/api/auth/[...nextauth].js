46343fc4c37b86996621a45c4735d00323ca2024
Max <max.belenchuk@gmail.com>
2023-04-20 14:00:07 -0300

    using email providers and credentials for auth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,20 @@
 
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email"
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
     // ...add more providers here
   ],
