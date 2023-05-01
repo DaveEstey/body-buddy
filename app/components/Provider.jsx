5981cdb566cc6c3d1f4469180f7e28a02a2ba18c
DaveEstey <114950818+DaveEstey@users.noreply.github.com>
2023-05-01 20:57:46 -0300

    made provider auth

---
+++ app/components/Provider.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { SessionProvider } from 'next-auth/react'
+
+const Provider = ({children}) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
+export default Provider;
+
+
